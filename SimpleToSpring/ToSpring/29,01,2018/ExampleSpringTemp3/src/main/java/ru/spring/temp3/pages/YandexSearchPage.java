3e65dcd7ce264c1d331218745ce235577293f12c
Vika <tempvika@bk.ru>
2018-01-29 20:21:43 +0300

    29.01.2018

---
+++ SimpleToSpring/ToSpring/29,01,2018/ExampleSpringTemp3/src/main/java/ru/spring/temp3/pages/YandexSearchPage.java
@@ -0,0 +1,51 @@
+package ru.spring.temp3.pages;
+
+//import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+/**
+ * Главная страница яндекса и переход в яндекс маркет
+ */
+public class YandexSearchPage extends AbstractPage {
+    private WebDriverWait wait;
+    private String URL = "http://yandex.ru";
+
+    @FindBy(xpath = "//*[contains(@class, 'home-tabs__search')]")
+    private List<WebElement> homeTabs;
+
+    public YandexSearchPage(){
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    /**
+     * Загрузка главной страницы яндекса
+     * @return этот объект
+     */
+    //@Step
+    public YandexSearchPage load(){
+        driver.navigate().to(URL);
+        //wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//")));
+
+        return this;
+    }
+
+    /**
+     * Перейти в маркет
+     * @return этот объект
+     */
+    //@Step
+    public YandexSearchPage goToMarket(){
+        for (WebElement element : homeTabs){
+            if (element.getText().equals("Маркет")) {
+                element.click();
+                break;
+            }
+        }
+
+        return this;
+    }
+}
---
+++ SimpleToSpring/ToSpring/29,01,2018/ExampleSpringTemp3/src/test/java/ru/spring/temp3/Execute.java
@@ -0,0 +1,44 @@
+package ru.spring.temp3;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.spring.temp3.helpers.DriverManager;
+import ru.spring.temp3.pages.YandexSearchPage;
+
+public class Execute {
+    /*public static void main(String [] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        *//**
+         * Пример применения Spring
+         * Источник:http://dev-blogs.com/simple-spring-app/
+         *//*
+        //Print printRectangle = (Print) context.getBean("printRectangle");
+        //printRectangle.showSquare();
+
+        //Print printCircle = (Print) context.getBean("printCircle");
+        //printCircle.showSquare();
+
+        DriverManager driverManager = (DriverManager) context.getBean("DriverManager");
+        DriverManager.getDriver();
+
+        YandexSearchPage yandexSearchPage = (YandexSearchPage) context.getBean("YandexSearchPage");
+        yandexSearchPage.load();
+        yandexSearchPage.goToMarket();
+
+    }*/
+
+    @Test
+    public void test1(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //ApplicationContextHolder.getBean(WebDriver.class);
+        DriverManager driverManager = (DriverManager) context.getBean("DriverManager");
+        //DriverManager.getDriver();
+
+        YandexSearchPage yandexSearchPage = (YandexSearchPage) context.getBean("YandexSearchPage");
+        yandexSearchPage.load();
+        yandexSearchPage.goToMarket();
+    }
+}
